3300115f30303e90fcc7956f6e0c86c6d535bb90
Ariele Bonifacio <arielebonifacio@gmail.com>
2022-12-11 00:12:02 +0100

    form validation

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -3,7 +3,12 @@ import styles from './Comment.module.css';
 import { Trash, ThumbsUp } from "phosphor-react";
 
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
     return(
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://github.com/bonieasy.png" />
@@ -14,7 +19,7 @@ export function Comment({ content }) {
                             <strong>Ariele Bonifacio</strong>
                             <time title='07 de Dezembro às 08:13h' dateTime="2022-12-07">Cerca de 1h atrás</time>
                         </div>
-                        <button title="delete comment">
+                        <button onClick={handleDeleteComment} title="delete comment">
                             <Trash size={24} />
                         </button>
                     </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,8 +7,10 @@ import styles from './Post.module.css';
 export function Post({ author, publishedAt, content }) {
 
     const [comments, setComments] = useState([
-        'Post Muito bacana'
+        'Post Muito bacana!'
     ])
+
+    const [newCommentText, setNewCommentText] = useState('')
 
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'")
 
@@ -17,15 +19,38 @@ export function Post({ author, publishedAt, content }) {
     })
 
     function handleCreateNewComment() {
-        event.preventDefault()
+        event.preventDefault() //Previne que ocorra evento padrao.
 
         const newCommentText = event.target.comment.value
+        // EVENT = SUBMIT
+        // TARGET = QUEM DISPAROU O EVENTO (NESSE CASO O FORM)
+        // COMMENT - NOME DO COMPONENTE DENTRO DO FORM QUE EU QUERO PEGAR (PODERIA SER O USER, IDADE, QQ COISA)
+        // VALUE = A PROPRIEDADE DO ELEMENTO COMMENT QUE EU QUERO PEGAR (PODERIA SER O CLASSNAME, ID, PLACEHOLDER, ETC)
 
         setComments([...comments, newCommentText]);
-
-        event.target.comment.value = '';
+        setNewCommentText('');
     }
 
+    function handleNewCommentChange () {
+        event.target.setCustomValidity('')
+        setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid() {
+        event.target.setCustomValidity('Esse campo é obrigatório')
+    }
+
+    function deleteComment(commentToDelete) {
+        //console.log(`deletar comentario ${comment}`)
+        const commentsWithoutDeletedOne = comments.filter(comment => {
+            return comment !== commentToDelete;
+        })
+
+        setComments(commentsWithoutDeletedOne);
+    }
+
+    const isNewCommentEmpty = newCommentText.length === 0;
+    
     return(
         <article className={styles.post}>
             <header>
@@ -47,19 +72,24 @@ export function Post({ author, publishedAt, content }) {
                     }
                 })}
             </div>
-            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
+            <form onAbort="{handleCleanEvent}" onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
+               
                 <textarea
                     name='comment'
                     placeholder="Deixe um comentário"
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
                 <footer>
-                    <button type="submit">Comentar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>Comentar</button>
                 </footer>
             </form>
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment content={comment} />
+                    return <Comment content={comment} onDeleteComment={deleteComment} />
                 })}
             </div>
         </article>
